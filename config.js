387f353bfa1dea85b2aad430f93b016607bd07d1
datct <thanhdatcao38@gmail.com>
2026-09-12 16:10:59 +0700

    update BE url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  //API_BASE_URL: "https://restaurant-api-t6pq.onrender.com",
-  API_BASE_URL: "http://127.0.0.1:8000",
+  API_BASE_URL: "https://restaurant-api-t6pq.onrender.com",
+  //API_BASE_URL: "http://127.0.0.1:8000",
   DEFAULT_ROBOT: 1,
   GEMINI_WS_BASE: "wss://generativelanguage.googleapis.com/ws/google.ai.generativelanguage.v1beta.GenerativeService.BidiGenerateContentConstrained",
 
